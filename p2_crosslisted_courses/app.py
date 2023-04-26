d64b221cefb8c0a6b980741a3b90674ea46bb213
Jarry <jfall@drew.edu>
2023-04-26 11:30:38 -0400

    edit and add updates

--- p2_crosslisted_courses/app.py
+++ p2_crosslisted_courses/app.py
@@ -4,10 +4,13 @@ Committee (Designation, Committee Code, Committee Name, Committee Type)
 Faculty (Faculty Email, Faculty Name)
 Faculty-Committee(Committee Code, Faculty Name, Faculty Start Semester, Membership Type, Designation, Academic Year) 
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 # pip3 install flask-sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, ForeignKey, Integer, String
+
+
+
 
 app = Flask(__name__)
 
@@ -31,7 +34,7 @@ class Course_Attributes(db.Model):
 # Define routes
 @app.route('/course-attributes')
 def course_attributes():
-    course_attributes_list = Course_Attributes.query.order_by(course_attributes.attribute_code).all()
+    course_attributes_list = Course_Attributes.query.order_by(Course_Attributes.attribute_code).all()
     return render_template('course_attributes.html', course_attributes_list=course_attributes_list)
 
 # Define routes
@@ -59,6 +62,27 @@ def faculty_committee_all():
 def faculty_committee_last_five():
     faculty_committee_list = Faculty_Committee.query.order_by(Faculty_Committee.academic_year.desc()).limit(5).all()
     return render_template('faculty_committee_last_five.html', faculty_committee_list=faculty_committee_list)
+
+@app.route('/add_course_attributes', methods=['GET', 'POST'])
+def add_faculty():
+    if request.method == 'POST':
+        email = request.form['course_attributes_email']
+        name = request.form['course_attributes_name']
+        course_attributes = Course_Attributes(course_attributes_email=email, course_attributes_name=name)
+        db.session.add(course_attributes)
+        db.session.commit()
+        return redirect(url_for('course_attributes_list'))
+    return render_template('add_course_attributes.html')
+
+
+@app.route('/edit_course_attributes/<attribute_code>', methods=['GET', 'POST'])
+def edit_course_attributes(attribute_code):
+    course_attributes = Course_Attributes.query.get(attribute_code)
+    if request.method == 'POST':
+        course_attributes.course_attributes_name = request.form['course_attributes_name']
+        db.session.commit()
+        return redirect(url_for('course_attributes_list'))
+    return render_template('edit_course_attributes.html', course_attributes=course_attributes)
 
 if __name__ == '__main__':
     app.run(debug=True)
